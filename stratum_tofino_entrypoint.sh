05282827676566e87be20f7a123cf3ccba8c8452
David Gengenbach <info@davidgengenbach.de>
2021-01-16 00:55:22 +0100

    Add more documentation

--- stratum_tofino_entrypoint.sh
+++ stratum_tofino_entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -eux
 
+# To check: the number of hugepages per switch is 128?
 sysctl -w vm.nr_hugepages=1024
 mkdir /mnt/huge || true
 mount -t hugetlbfs nodev /mnt/huge || true
 
-veth_setup.sh 
+# The veths will be created WITHIN the Docker container - not the host system (because of bridged- instead of host-network)
+veth_setup.sh
 tofino-model --p4-target-config /usr/share/stratum/tofino_skip_p4.conf $@ &
-
 start-stratum.sh  -bf_sim -bf_switchd_background=false -enable_onlp=false
